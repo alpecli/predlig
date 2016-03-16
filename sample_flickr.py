fc007d1bfea763de1c30f1945efe5d97775a19c5
Antonio Pecli <alpecli@gmail.com>
2016-03-16 08:46:46 -0300

    Included some files with execution example with the flickr dataset.

---
+++ sample_flickr.py
@@ -0,0 +1,7 @@
+from sample import *
+
+sample = CDConstructor("flickr_al.txt", 10000, (0.5, 0.5))
+dataset = sample.get_sample()
+sample.save_sample('flickr_sample.txt')
+sample.get_classification_dataset()
+sample.save_classification_dataset('flickr_final_version')
